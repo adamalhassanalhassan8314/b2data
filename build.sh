6cbe40c9437adc5c680262239f3c6efb041e0454
alhassan <adamaalhassanalhassan8314@gmail.com>
2026-08-11 23:51:38 -0700

    Fix Render gunicorn startup

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput
+# python manage.py migrate
+
+
+#!/usr/bin/env bash
+
+python -m pip install -r requirements.txt
+python -m pip install gunicorn
 python manage.py collectstatic --noinput
 python manage.py migrate
